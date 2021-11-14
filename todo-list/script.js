c918f73e0021658f3508c279451487af3d5b1ac6
CarlosRuanOliveira <carlosruan.ti@gmail.com>
2021-11-13 21:28:25 -0300

    Update todo-list/script.js: renomeei algumas variaveis e refiz a funçao add, agora usando melhor as funçoes de manipulaçao do DOM para criar elementos e adicionar conteudo a pagina

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,31 +1,54 @@
-let taskCount = 1
-/* Detecta se a tecla enter é digitada no input */
+let taskCount = 0 // Seu valor é incrementado atribuído como o id da div ao criar adicionar uma tarefa
+// Detecta se a tecla enter é digitada no input
 function teclaEnter(){
-    input = document.getElementById("addTarefa").value
+    const inputValor = document.getElementById("addTarefa").value
     document.addEventListener('keydown', function (event) {    
-                                            if (event.keyCode === 13 && input) { /* Verifica tecla enter valor do input */
+                                            if (event.keyCode === 13 && inputValor) { /* Verifica tecla enter valor do input */
                                                 add()
                                             }
                                         }, {once: true})
 }
-/* Adiciona a tarefa na lista */
+// Adiciona a tarefa na lista
 function add(){
     taskCount++
-    const tarefa = document.getElementById("addTarefa").value
+    let input = document.getElementById("addTarefa")
     const fieldset = document.getElementById("tarefas")
-    fieldset.innerHTML += "<div class='itens' id='" + taskCount + "'><label class='item' onclick='checked(this)'>"+ tarefa +"<span class='checkmark'></span></label><span class='remove' onclick='remove(this);'></span></div>"
-    document.getElementById("addTarefa").value = ""
+
+    const div = document.createElement('div')
+    div.setAttribute('class', 'itens')
+    //div.setAttribute('id', localStorage.taskCount)
+    div.setAttribute('id', taskCount)
+
+    const label = document.createElement('label')
+    label.setAttribute('class', 'item')
+    label.setAttribute('onclick', 'checked(this)')
+
+    const spanCheckbox = document.createElement('label')
+    spanCheckbox.setAttribute('class', 'checkmark')
+
+    label.innerHTML = input.value
+    label.appendChild(spanCheckbox)
+
+    const spanRemove = document.createElement('label')
+    spanRemove.setAttribute('class', 'remove')
+    spanRemove.setAttribute('onclick', 'remove(this)')
+
+    div.appendChild(label)
+    div.appendChild(spanRemove)
+
+    fieldset.appendChild(div)
+
+    input.value = ""
 }
 /* Remove uma tarefa da lista */
-function remove(e) {
-    const pai = e.parentNode
-    if (pai.firstElementChild.classList =="item checked") {
+function remove(element) {
+    const pai = element.parentNode
+    if (pai.firstElementChild.classList == "item checked") {
         pai.remove()
     } else {
         alert("Para excluir uma tarefa, primeiro marque-a como concluída")
     }
 }
-function checked(e) {
-    const elemento = e
-    elemento.classList.toggle('checked')
+function checked(element) {
+    element.classList.toggle('checked')
 }
